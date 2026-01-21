2e91fd82613152204eb13df94b6ec42ad6b53035
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2026-01-20 19:32:10 -0500

    hello world

--- 1-Variables-IO-Math/SP26/HelloSpring.java
+++ 1-Variables-IO-Math/SP26/HelloSpring.java
@@ -1,5 +1,15 @@
-public class HelloSpring {
-    public static void main(String[] args) {
-
+/*
+    This is the very first program
+    that we are writing in CS-1180
+    in the Spring semester!!
+ */
+public class HelloSpring
+{
+    public static void main(String[] args)
+    {
+        // this next line is a print statement!!!
+        System.out.println("Hello Spring 2026 class!!"); // this is an in-line comment
+        // you can comment out code that is causing errors as a form of debugging
+        // getWord();
     }
 }
