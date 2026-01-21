3d00130876b516e3350fdefdbf96aae07c80907f
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2026-01-21 14:43:41 -0500

    variable intro

---
+++ 1-Variables-IO-Math/SP26/VariablePractice.java
@@ -0,0 +1,26 @@
+public class VariablePractice
+{
+    public static void main(String[] args)
+    {
+        // variable declaration
+        int numOfStudents;
+
+        // variable assignment (initialization)
+        numOfStudents = 8;
+
+        System.out.println(numOfStudents);
+
+        // combining declaration and assignment (initialization) into one line\
+        // booleans can only hold true or false
+        // true and false are java keywords
+        boolean isPresent = true;
+
+        isPresent = false;
+
+        System.out.println(isPresent);
+
+        // char can only hold a SINGLE number, letter, or symbol
+        char letterGrade = 'A';
+        System.out.println(letterGrade);
+    }
+}
