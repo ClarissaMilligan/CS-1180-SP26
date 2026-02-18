b76ca342621e007ad9e5e156c73a1aca9cfcdbda
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2026-02-18 14:22:02 -0500

    method practice

--- 5-Methods/SP26/Methods.java
+++ 5-Methods/SP26/Methods.java
@@ -1,5 +1,21 @@
+import java.util.Random;
+
 public class Methods {
     public static void main(String[] args) {
+        int goodDeeds = 10;
+        // class name, Methods, is not required because the method, isLucky,
+        // is defined in the same class that it is called from
+        System.out.println(Methods.isLucky(goodDeeds));
+    }
 
+    public static boolean isLucky(int numGoodDeeds) {
+        Random rng = new Random();
+        int randomInt = rng.nextInt(11);
+        int luckValue = numGoodDeeds * randomInt;
+
+        if (luckValue > 50) {
+            return true;
+        }
+        return false;
     }
 }
