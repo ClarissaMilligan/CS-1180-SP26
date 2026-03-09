e7c7e4ca90618f3e2a92b1e587f0080be002dbd9
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2026-03-09 14:20:10 -0400

    intro to file writing

--- 6-File-IO/SP26/FileIOIntro.java
+++ 6-File-IO/SP26/FileIOIntro.java
@@ -1,5 +1,16 @@
-public class FileIOIntro {
-    public static void main(String[] args) {
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+public class FileIOIntro
+{
+    public static void main(String[] args) throws IOException
+    {
+        FileWriter appendToFile = new FileWriter("findWorkingDirectory.txt", true);
+        PrintWriter writeToFile = new PrintWriter(appendToFile);
+        writeToFile.println("I hope I feel better soon!");
+        writeToFile.close();
+        appendToFile.close();
     }
 }
