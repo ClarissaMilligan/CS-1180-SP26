4957e49a1bcd7343c34a15884bb098aec4fae7cf
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2026-03-30 14:23:11 -0400

    basic class discussion

--- 8-Objects/SP26/ObjectsIntro.java
+++ 8-Objects/SP26/ObjectsIntro.java
@@ -1,2 +1,8 @@
-public class ObjectsIntro {
+public class ObjectsIntro
+{
+    public static void main(String[] args)
+    {
+        Pixel p1 = new Pixel();
+        p1.pixelInfo();
+    }
 }
---
+++ 8-Objects/SP26/Pixel.java
@@ -0,0 +1,11 @@
+public class Pixel
+{
+    int x = 0;
+    int y = 0;
+    String color = "";
+
+    public void pixelInfo()
+    {
+        System.out.println(color + " pixel at x:" + x + " y:" + y);
+    }
+}
