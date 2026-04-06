ffc4bc8be35b1afd50ae9e4f5515eda71a266e6f
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2026-04-06 14:23:58 -0400

    constructors and encapsulation

--- 8-Objects/SP26/ObjectsIntro.java
+++ 8-Objects/SP26/ObjectsIntro.java
@@ -2,7 +2,33 @@ public class ObjectsIntro
 {
     public static void main(String[] args)
     {
-        Pixel p1 = new Pixel();
+        Pixel p1 = new Pixel(33, 89, "blue");
+        Pixel p2 = new Pixel();
+
         p1.pixelInfo();
+        p2.pixelInfo();
+
+//        System.out.println(p1.x);
+//        p1.x = 72;
+//        System.out.println(p1.x);
+//        p1.x = -42387;
+//        System.out.println(p1.x);
+
+        System.out.println(p1.getX());
+        p1.setX(72);
+        System.out.println(p1.getX());
+        p1.setX(-42387);
+        System.out.println(p1.getX());
+
+        System.out.println(p1.setY(-40));
+        System.out.println(p1.getY());
+
+        p1.setColor("PURPLE");
+        System.out.println(p1.getColor());
+
+        Pixel p3 = new Pixel(-45, 90000, "cyAN");
+        p3.pixelInfo();
+
+        p3 = new Pixel();
     }
 }
--- 8-Objects/SP26/Pixel.java
+++ 8-Objects/SP26/Pixel.java
@@ -1,11 +1,88 @@
+import java.util.*;
+
 public class Pixel
 {
-    int x = 0;
-    int y = 0;
-    String color = "";
+    private int x = 0;
+    private int y = 0;
+    private String color = "";
+
+    private int maxX = 1920;
+    private int maxY = 1080;
+    private ArrayList<String> validColors = new ArrayList<>(Arrays.asList("red", "orange", "yellow", "green", "blue", "purple", "pink", "black", "white", "gray", "brown"));
+
+    public Pixel()
+    {
+//        x = 0;
+//        y = 0;
+//        color = "";
+    }
+
+    public Pixel(int x, int y, String newPixelColor)
+    {
+//        this.x = x;
+//        this.y = y;
+//        color = newPixelColor;
+        // TODO: make this throw custom exceptions if values out of bounds
+        setX(x);
+        setY(y);
+        setColor(color);
+    }
 
     public void pixelInfo()
     {
         System.out.println(color + " pixel at x:" + x + " y:" + y);
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public void setX(int newX)
+    {
+        if (newX >= 0 && newX <= maxX)
+        {
+            x = newX;
+        }
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public boolean setY(int newY)
+    {
+        if (newY >= 0 && newY <= maxY)
+        {
+            y = newY;
+            return true;
+        }
+        return false;
+    }
+
+    public String getColor()
+    {
+        return color;
+    }
+
+    public void setColor(String color)
+    {
+        color = color.toLowerCase();
+        if (validColors.contains(color))
+        {
+            this.color = color;
+        }
+
+    }
+
+    public int getMaxX()
+    {
+        return maxX;
+    }
+
+    public int getMaxY()
+    {
+        return maxY;
     }
 }
